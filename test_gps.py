42281d669b9a1a160d416aec6a72e5935dd919e7
Jeff Wiedemann <jeffr@2jeffwiedemann.com>
2019-01-05 19:55:08 +0000

    parses gga for alt

--- test_gps.py
+++ test_gps.py
@@ -6,7 +6,23 @@ port = "/dev/serial0"
 
 ser = serial.Serial(port, baudrate = 57600, timeout = 0.5)
 
-print "Receiving GPS data"
+#def lat_long_decode(coord):
+#    #Converts DDDMM.MMMMM > DD deg MM.MMMMM min
+#    x = coord.split(".")
+#    head = x[0]
+#    tail = x[1]
+#    deg = head[0:-2]
+#    min = head[-2:]cd gp
+#    return deg + " deg " + min + "." + tail + " min"
+######## end of lat_long_decode ##############
+
+
+def parse_gga(sdata):
+     alt = sdata[7]
+     return alt
+######## end of parse gga  ##############
+
+print "Looking for GPS data"
 
 
 count = 1
@@ -14,6 +30,10 @@ count = 1
 while True:
    data = ser.read_until() 
    sdata = data.split(",")
+
+   if sdata[0] == "$GPGGA":
+       alt = parse_gga(sdata)
+
 
    if sdata[0] == "$GPRMC":
        if sdata[2] == 'V':
@@ -24,13 +44,9 @@ while True:
           lat_dir = sdata[4]
           long = sdata[5]
           long_dir = sdata[6]
-   if sdata[0] == "$GPGGA"
-          alt = sdata[7]
-
-
           print count
           count = count + 1
-          print data
+          print alt
 
 
 
