51277b9c31f61a350725a72aaa50fb4d5d75131a
Jeff Wiedemann <jeffr@2jeffwiedemann.com>
2019-01-06 18:45:32 +0000

    gets all of fix data from gga

--- gonet.py
+++ gonet.py
@@ -3,7 +3,7 @@
 import serial
  
 most_of_gps = ""
-alt = ""
+date = ""
 
 port = "/dev/serial0"
 ser = serial.Serial(port, baudrate = 57600, timeout = 0.5)
@@ -15,26 +15,29 @@ ser = serial.Serial(port, baudrate = 57600, timeout = 0.5)
 #    tail = x[1]
 #    deg = head[0:-2]
 #    min = head[-2:]cd gp
+#
 #    return deg + " deg " + min + "." + tail + " min"
+
 ######## end of lat_long_decode ##############
 
 
 def parse_gga(sdata):
+     time = sdata[1][0:2] + ":" + sdata[1][2:4] + ":" + sdata[1][4:6]
+     lat = sdata[2]
+     lat_dir = sdata[3]
+     long = sdata[4]
+     long_dir = sdata[5]
      alt = sdata[9]
-     return alt
+
+     return time + " " +  lat + " " + lat_dir + " " + long + " " + long_dir + " " + alt + " M"
+
 ######## end of parse gga  ##############
 
 
 def parse_rmc(sdata):
      date =data[9][0:2] + "/" + sdata[9][2:4] + "/" + sdata[9][4:6]
-     time = sdata[1][0:2] + ":" + sdata[1][2:4] + ":" + sdata[1][4:6]
-     lat = sdata[3]
-     lat_dir = sdata[4]
-     long = sdata[5]
-     long_dir = sdata[6]
-     
-     return date + " " + time + " " +  lat + " " + lat_dir + " " + long + " " + long_dir
-     #print  date + " " + time + " " +  lat + " " + lat_dir + " " + long + " " + long_dir
+ 
+     return date
 ####### end of parse rmc  ##############
 
 
@@ -48,21 +51,21 @@ while True:
    sdata = data.split(",")
 
    if sdata[0] == "$GPGGA":
-       alt = parse_gga(sdata)
+       most_of_gps  = parse_gga(sdata)
 
    if sdata[0] == "$GPRMC":
 #       if sdata[2] == 'V':
 #          print "no satellite data available"
 # this would be a great place for an else           
 
-          most_of_gps = parse_rmc(sdata)
+          date = parse_rmc(sdata)
           break 
 
 
 
 
 
-gps_string =  most_of_gps + " " + alt
+gps_string = date + " " + most_of_gps
 print gps_string
 
 
